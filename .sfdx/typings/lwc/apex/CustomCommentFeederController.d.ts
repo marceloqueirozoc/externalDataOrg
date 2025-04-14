f27929927ab9c5fb3705f760d6514ee1e98fee59
marceloconstantino <marcelo.constantino@everymind.com.br>
2025-04-14 14:07:43 -0300

    Versionamento de Metadata

---
+++ .sfdx/typings/lwc/apex/CustomCommentFeederController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/CustomCommentFeederController.getFeedItemList" {
+  export default function getFeedItemList(param: {status: any, parentId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/CustomCommentFeederController.createFeedItemRec" {
+  export default function createFeedItemRec(param: {feedItemRec: any}): Promise<any>;
+}
